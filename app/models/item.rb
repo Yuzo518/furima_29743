2c3d33d1464aefb8c80dbec0723420d157490b3d
Yuzo <hirakawa.yuzo@gmail.com>
2020-09-17 12:18:15 +0900

    ActiveStorageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category, :status, :area, :delivery_fee, :date_of_shipment
   belongs_to :user
+  has_one_attached :image
 
   validates :name, :value, :comment, :category, :status, :area, :delivery_fee, :date_of_shipment
 
