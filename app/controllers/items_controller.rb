d2e89f614118432a7dda84359e8fcb84b8948b7b
Yuzo <hirakawa.yuzo@gmail.com>
2020-09-17 15:41:55 +0900

    商品の保存機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,29 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :comment,
+      :value,
+      :category_id,
+      :status_id,
+      :delivery_fee_id,
+      :area_id,
+      :date_of_shipment_id,
+      :image
+    ).merge(user_id: current_user.id)
+  end
 end
