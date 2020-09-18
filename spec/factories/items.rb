05037e63e3539566cc7f53e3c0adb8ba4e1a1367
Yuzo <hirakawa.yuzo@gmail.com>
2020-09-18 15:51:44 +0900

    itemモデル単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name                { "ItemaName" }
+    value               { 3000 }
+    comment             { "ItemeComment" }
+    category_id         { 2 }
+    status_id           { 2 }
+    date_of_shipment_id { 2 }
+    area_id             { 2 }
+    delivery_fee_id     { 2 }
+    association         :user
+
+    after(:build) do |i|
+      i.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
