ca6f8db1dda4003875c0aa4ab093ba459fe8802c
Yuzo <hirakawa.yuzo@gmail.com>
2020-09-22 19:21:50 +0900

    購入ページの表示

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,8 +1,9 @@
 class BuysController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
   end
-  
+
   def new
   end
 
--- app/models/buy.rb
+++ app/models/buy.rb
@@ -1,4 +1,7 @@
 class Buy < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefectures
+
   belongs_to :user
   belongs_to :item
   has_one :address
