711ccb2502182d6b87ee0f2fca5e2069d415ae4d
Yuzo <hirakawa.yuzo@gmail.com>
2020-09-23 12:39:43 +0900

    クレジット決済機能の実装

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -8,6 +8,31 @@ class BuysController < ApplicationController
   end
 
   def create
+    @buy = Buy.new(item_id: buy_params[:item_id], user_id: current_user.id)
+    if @buy.valid?
+      pay_item
+      @buy.save
+      return redirect_to root_path
+    else
+      return 'index'
+    end
+  end
+
+  private
+
+  def buy_params
+    params.permit(:item_id, :token)
+  end
+
+  def pay_item
+    # buy_paramsメソッドより取得したitem_idより指定のitemを取得する
+    item = Item.find(buy_params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: item.value,
+      card: buy_params[:token],
+      currency:'jpy'
+    )
   end
 
 end
--- app/models/buy.rb
+++ app/models/buy.rb
@@ -1,7 +1,4 @@
 class Buy < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefectures
-
   belongs_to :user
   belongs_to :item
   has_one :address
